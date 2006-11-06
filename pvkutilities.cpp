c631e499a98807a88621398e3d42ace311d4be90
Peter van Kranenburg <7080628+pvankranenburg@users.noreply.github.com>
2006-11-06 15:34:44 +0000

    new wce2krn snapshot

---
+++ pvkutilities.cpp
@@ -0,0 +1,16 @@
+/*
+ *  pvkutilities.cpp
+ *  wce2krn
+ *
+ *  Created by Peter Van Kranenburg on 11/3/06.
+ *  Copyright 2006 Peter van Kranenburg. All rights reserved.
+ *
+ */
+
+#include "pvkutilities.h"
+
+string& pvktrim(string& s, string drop) {
+	s.erase(s.find_last_not_of(drop)+1);
+    s.erase(0,s.find_first_not_of(drop));
+	return s;
+}
