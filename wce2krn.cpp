c83fc3ccdbe69ed6a5d26aa45586482ea43588dc
Peter van Kranenburg <7080628+pvankranenburg@users.noreply.github.com>
2007-01-04 16:43:05 +0000

    to_accent added

--- wce2krn.cpp
+++ wce2krn.cpp
@@ -10,7 +10,6 @@
 #include <iostream>
 #include <string>
 #include "Song.h"
-//#include <cmdargs.h>
 using namespace std;
 
 #include "SongLine.h"
@@ -18,7 +17,6 @@ using namespace std;
 void usage() {
 	cerr << "Usage: wce2krn [-k] [-s] [-l] [wcefile]" << endl;
 	cerr << "Reads wce-file and generates *kern file" << endl;
-	//cerr << " -k: generate *kern." << endl;
 	cerr << " -s: spit in lines." << endl;
 	cerr << " -l: generate file(s) with only the lyrics." << endl;
 	cerr << " -h: print this help message" << endl;
@@ -28,28 +26,6 @@ void usage() {
 
 int main (int argc, char * const argv[]) {
 	
-	/*
-	// Commandline arguments
-	CmdArgBool split_flag('s', "split", "generate an separate outputfile for each verse.");
-	//CmdArgInt  wnumber('w', "window", "number", "size of the window when splitting.");
-	//CmdArgInt  onumber('o', "offset", "number", "stepsize to next part when splitting.");
-	//CmdArgBool  mflag('m', "mason", "make mason diagram. Will be written in png-format");
-	CmdArgBool  text_flag('t', "text", "generate a with only the lyrics.");
-	CmdArgStr  input("input-file",  "input file. A WichcraftEditor file.");
-	//CmdArgStr  output("output-file",  "file to write featurevalues.");
-
-	// Declare command object and its argument-iterator
-	CmdLine  cmd(*argv, &split_flag, &text_flag, &input, NULL);
-	CmdArgvIter arg_iter(--argc, ++argv);
-
-	// Initialize arguments to appropriate default values.
-	split_flag = 0;
-	text_flag = 0;
-  
-	// Parse arguments
-	cmd.parse(arg_iter);	
-	*/
-	
 	bool kern = true;
 	bool split = false;
 	bool absolute = true;
@@ -57,8 +33,6 @@ int main (int argc, char * const argv[]) {
 	
 	string filename = "-";
 	string arg = "";
-	//string arg1 = "";
-	//string arg2 = "";
 	
 	for (int i = 1; i<argc; i++ ) {
 		arg = string(argv[i]);
@@ -68,30 +42,9 @@ int main (int argc, char * const argv[]) {
 		else filename = arg;
 	}
 	
-	/*
-	if ( argc == 4 ) {
-		filename = string(argv[3]);
-		arg1 = string(argv[1]);
-		arg2 = string(argv[2]);		
-	} else if ( argc == 3 ) {
-		filename = string(argv[2]);
-		arg1 = string(argv[1]);
-	} else if ( argc == 2 ) {
-		filename = string(argv[1]);
-	} else {
-		usage();
-		exit(1);
-	}
-	
-	if ( arg1 == "-s" || arg2 == "-s" ) split = true;
-	if ( arg1 == "-l" || arg2 == "-l" ) lyrics = true;
-	*/
-	
 	clog << endl;
 	clog << "Processing " << filename << endl;
 	
-	
-	//const std::string filename = std::string(input);
 	string basename = filename;
 	
 	if ( basename != "-" ){
