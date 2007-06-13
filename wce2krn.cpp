1af1d174345eb69bacbcacd36efec176332cbad4
Peter van Kranenburg <7080628+pvankranenburg@users.noreply.github.com>
2007-06-13 19:38:57 +0000

    added lilypond output to wce2krn.

--- wce2krn.cpp
+++ wce2krn.cpp
@@ -15,21 +15,25 @@ using namespace std;
 #include "SongLine.h"
 
 void usage() {
-	cerr << "Usage: wce2krn [-k] [-s] [-l] [wcefile]" << endl;
+	cerr << "Usage: wce2krn [-k] [-r] [-s] [-l] [-h] [wcefile]" << endl;
 	cerr << "Reads wce-file and generates *kern file" << endl;
-	cerr << " -s: spit in lines." << endl;
+	cerr << " -k: generate kernfile." << endl;
+	cerr << " -r: generate relative lilypond file." << endl;
+	cerr << " -a: generate absolute lilypond file (not implemented)!" << endl;
 	cerr << " -l: generate file(s) with only the lyrics." << endl;
+	cerr << " -s: spit in lines." << endl;
 	cerr << " -h: print this help message" << endl;
 	cerr << "If no filename is given, or '-', standard input and output will be used." << endl;
-	cerr << "In this case, '-s' doesn't have any effect and '-l' suppresses *kern output." << endl << endl;
+	cerr << "In this case, '-s' doesn't have any effect." << endl << endl;
 }
 
 int main (int argc, char * const argv[]) {
 	
-	bool kern = true;
+	bool kern = false;
 	bool split = false;
-	bool absolute = true;
 	bool lyrics = false;
+	bool absly = false;
+	bool relly = false;
 	
 	string filename = "-";
 	string arg = "";
@@ -37,7 +41,10 @@ int main (int argc, char * const argv[]) {
 	for (int i = 1; i<argc; i++ ) {
 		arg = string(argv[i]);
 		if ( arg == "-s" ) split = true;
+		else if ( arg == "-k" ) kern = true;
 		else if ( arg == "-l" ) lyrics = true;
+		else if ( arg == "-r" ) relly = true;
+		else if ( arg == "-a" ) absly = true;
 		else if ( arg == "-h" ) { usage(); exit(0); }
 		else filename = arg;
 	}
@@ -57,8 +64,10 @@ int main (int argc, char * const argv[]) {
 	
 	if (basename == "-" && kern && lyrics) kern = false;
 	if (basename == "-" && split ) split = false;
-	if (kern) s.writeToDisk(basename, SongLine::KERN, split, absolute);
-	if (lyrics) s.writeToDisk(basename, SongLine::TEXT, split, absolute);
+	if (kern) s.writeToDisk(basename, SongLine::KERN, split);
+	if (lyrics) s.writeToDisk(basename, SongLine::TEXT, split);
+	if (relly) s.writeToDisk(basename, SongLine::RELLY, split);
+	//if (absly) s.writeToDisk(basename, SongLine::ABSLY, split);
 	
 	return 0;
 }
