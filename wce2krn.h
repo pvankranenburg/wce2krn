776df064175d912f53bd4e3351f0d8afec2704da
Peter van Kranenburg <7080628+pvankranenburg@users.noreply.github.com>
2008-06-19 09:40:52 +0000

    wce2krn update

--- wce2krn.h
+++ wce2krn.h
@@ -13,7 +13,7 @@
 #include <string>
 using namespace std;
 
-const string version = "1.40";
-const string releasedate = "15 June 2008";
+const string version = "1.40.1";
+const string releasedate = "19 June 2008";
 
 #endif
